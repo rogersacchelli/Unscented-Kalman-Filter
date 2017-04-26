54aa68532adfd36550d0b7f5332e2ae4a05d0aa5
Roger Sacchelli <roger.sacchelli@gmail.com>
2017-04-26 00:22:34 -0300

    Update ukf.h

--- src/ukf.h
+++ src/ukf.h
@@ -35,6 +35,31 @@ public:
   void PredictRadarMeasurement(VectorXd* z_out, MatrixXd* S_out);
   void UpdateState(VectorXd* x_out, MatrixXd* P_out);
 
+  /**
+   * ProcessMeasurement
+   * @param meas_package The latest measurement data of either radar or laser
+   */
+  void ProcessMeasurement(MeasurementPackage meas_package);
+
+  /**
+   * Prediction Predicts sigma points, the state, and the state covariance
+   * matrix
+   * @param delta_t Time between k and k+1 in s
+   */
+  void Prediction(double delta_t);
+
+  /**
+   * Updates the state and the state covariance matrix using a laser measurement
+   * @param meas_package The measurement at k+1
+   */
+  void UpdateLidar(MeasurementPackage meas_package);
+
+  /**
+   * Updates the state and the state covariance matrix using a radar measurement
+   * @param meas_package The measurement at k+1
+   */
+  void UpdateRadar(MeasurementPackage meas_package);
+
 };
 
 #endif /* UKF_H */
